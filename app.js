19811a3a7624a9b949df3abc6e61c4bcccf50f79
SofaKB <hall.b17@gmail.com>
2020-02-28 20:27:53 -0500

    added questions.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const person = require('./lib/person');
 const manager = require('./lib/manager');
 const engineer = require('./lib/engineer');
 const intern = require('./lib/intern');
+const questions = require('./lib/questions')
 
 const createHTML = require('./templates/createHTML');
 const createManager = require('./templates/createManager');
