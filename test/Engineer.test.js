811e4a640dd744d28377c4d604478c7f8bed85e0
SofaKB <hall.b17@gmail.com>
2020-02-28 23:21:50 -0500

    test fix

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,9 +1,9 @@
-const Engineer = require("../lib/Engineer");
+const Engineer = require("../lib/engineer");
 
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.github).toBe(testValue);
+  expect(e.git).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,10 +1,9 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Manager = require("../lib/manager");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
+  expect(e.phoneNumber).toBe(testValue);
 });
 
 test("getRole() should return \"Manager\"", () => {
@@ -16,5 +15,5 @@ test("getRole() should return \"Manager\"", () => {
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+  expect(e.getPhoneNumber()).toBe(testValue);
 });
