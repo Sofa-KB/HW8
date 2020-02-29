912cccb2d9dad0a8dcc83f18c2acca570b4d950f
SofaKB <hall.b17@gmail.com>
2020-02-28 22:44:51 -0500

    final

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const manager = require('./lib/manager');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 const questions = require('./lib/questions')
 
 const createHTML = require('./templates/createHTML');
 const createManager = require('./templates/createManager');
 const createEngineer = require('./templates/createEngineer');
 const createIntern = require('./templates/createIntern');
+
+async function create() {
+    try {
+        let id = 1;
+        let personCards = "";
+        let add = true;
+
+        while(add) {
+            const personInfo = await inquirer.prompt(questions.person)
+            switch(personInfo.role) {
+                case "Manager":
+                    const managerInfo = await inquirer.prompt(questions.manager);
+                    const manager = new Manager(personInfo.name, id, `${personInfo.email.replace(/\s/g, '')}`, managerInfo.phoneNumber);
+                    personCards += createManager(manager);
+                    break;
+                case "Engineer":
+                    const engineerInfo = await inquirer.prompt(questions.engineer);
+                    const engineer = new Engineer(personInfo.name, id, `${personInfo.email.replace(/\s/g, '')}`, engineerInfo.git);
+                    personCards += createEngineer(engineer);
+                    break;
+                case "Intern": 
+                    const internInfo = await inquirer.prompt(questions.intern);
+                    const intern = new Intern(personInfo.name, id, `${personInfo.email.replace(/\s/g, '')}`, internInfo.school);
+                    personCards += createIntern(intern);
+                    break;    
+            }
+            const addPerson = await inquirer.prompt(questions.addPerson)
+            if(!addPerson.true) add = false;
+            id++
+        }
+
+        fs.writeFile('./generated/generated.html', createHTML(personCards), function(err){
+            if(err) throw err
+            console.log('File created successfully!')
+            console.log(personCards)
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+create()
--- templates/createEngineer.js
+++ templates/createEngineer.js
@@ -1,14 +1,15 @@
 function createEngineer(data) {
     return `
     <div class="card col-xs-12 col-sm-12 col-md-5 col-lg-3">
-        <div>
+        <div class='text-center'>
             <h1>${data.name}</h1>
             <h2>Engineer</h2>
         </div>
+        <br>
         <ul>
-            <li>ID: ${data.id}</li>
-            <li>Email: ${data.email}</li>
-            <li>GitHub: ${data.github}</li>
+            <li><strong>ID:</strong> ${data.id}</li>
+            <li><strong>Email:</strong> ${data.email}</li>
+            <li><strong>GitHub:</strong> ${data.git}</li>
         </ul>
     </div>
     `
